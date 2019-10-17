f8b982a6770a93310b5c4c605c741a09b6ef6a84
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-10-16 20:48:56 -0400

    Adding event schedule items to Netlify CMS config.

--- src/components/seo.js
+++ src/components/seo.js
@@ -39,56 +39,6 @@ function SEO({ description, lang, meta, title, pathname }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      // meta={[
-      //   {
-      //     name: `description`,
-      //     content: metaDescription,
-      //   },
-      //   {
-      //     name: `keywords`,
-      //     content: site.siteMetadata.keywords,
-      //   },
-      //   {
-      //     property: `og:title`,
-      //     content: title,
-      //   },
-      //   {
-      //     property: `og:description`,
-      //     content: metaDescription,
-      //   },
-      //   {
-      //     property: `og:url`,
-      //     content: pageUrl,
-      //   },
-      //   {
-      //     property: `og:image`,
-      //     content: ogImageUrl,
-      //   },
-      //   {
-      //     property: `og:type`,
-      //     content: `website`,
-      //   },
-      //   {
-      //     name: `twitter:card`,
-      //     content: `summary`,
-      //   },
-      //   {
-      //     name: `twitter:image`,
-      //     content: ogImageUrl,
-      //   },
-      //   {
-      //     name: `twitter:creator`,
-      //     content: site.siteMetadata.author,
-      //   },
-      //   {
-      //     name: `twitter:title`,
-      //     content: title,
-      //   },
-      //   {
-      //     name: `twitter:description`,
-      //     content: metaDescription,
-      //   },
-      // ].concat(meta)}
     >
             {/* General tags */}
             <title>{title}</title>
