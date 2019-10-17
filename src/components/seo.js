2602d69c8b4cb2f985ddebea44000910c954855e
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-10-16 20:25:18 -0400

    Trying new meta tag method.

--- src/components/seo.js
+++ src/components/seo.js
@@ -39,57 +39,80 @@ function SEO({ description, lang, meta, title, pathname }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          name: `keywords`,
-          content: site.siteMetadata.keywords,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:url`,
-          content: pageUrl,
-        },
-        {
-          property: `og:image`,
-          content: ogImageUrl,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:image`,
-          content: ogImageUrl,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
+      // meta={[
+      //   {
+      //     name: `description`,
+      //     content: metaDescription,
+      //   },
+      //   {
+      //     name: `keywords`,
+      //     content: site.siteMetadata.keywords,
+      //   },
+      //   {
+      //     property: `og:title`,
+      //     content: title,
+      //   },
+      //   {
+      //     property: `og:description`,
+      //     content: metaDescription,
+      //   },
+      //   {
+      //     property: `og:url`,
+      //     content: pageUrl,
+      //   },
+      //   {
+      //     property: `og:image`,
+      //     content: ogImageUrl,
+      //   },
+      //   {
+      //     property: `og:type`,
+      //     content: `website`,
+      //   },
+      //   {
+      //     name: `twitter:card`,
+      //     content: `summary`,
+      //   },
+      //   {
+      //     name: `twitter:image`,
+      //     content: ogImageUrl,
+      //   },
+      //   {
+      //     name: `twitter:creator`,
+      //     content: site.siteMetadata.author,
+      //   },
+      //   {
+      //     name: `twitter:title`,
+      //     content: title,
+      //   },
+      //   {
+      //     name: `twitter:description`,
+      //     content: metaDescription,
+      //   },
+      // ].concat(meta)}
+    >
+            {/* General tags */}
+            <title>{title}</title>
+            <meta name="description" content={metaDescription} />
+            <meta name="image" content={ogImageUrl} />
+            <link rel="canonical" href={pageUrl} />
+
+            {/* OpenGraph tags */}
+            <meta property="og:url" content={pageUrl} />
+            {/* {isBlogPost ? <meta property="og:type" content="article" /> : null} */}
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={metaDescription} />
+            <meta property="og:image" content={ogImageUrl} />
+            {/* <meta property="fb:app_id" content={seo.social.fbAppID} /> */}
+
+
+            {/* Twitter Card tags */}
+            <meta name="twitter:card" content="summary_large_image" />
+            {/* <meta name="twitter:creator" content={seo.social.twitter} /> */}
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={metaDescription} />
+            <meta name="twitter:image" content={ogImageUrl} />
+
+    </Helmet>
   )
 }
 
